df11f3c06c39afb3b2c7334a510ea45e69bba1aa
Varistus47 <106617396+Varistus47@users.noreply.github.com>
2022-11-20 23:02:27 +0530

    Add files via upload

---
+++ star.py
@@ -0,0 +1,14 @@
+from turtle import *
+goto(-150,0)
+bgcolor("black")
+color("yellow")
+speed(0.6)
+begin_fill()
+for i in range(1,5):
+    forward(250)
+    left(145)
+end_fill()
+hideturtle()
+done()
+
+
