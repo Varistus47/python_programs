a01f0c0b8ca89ef44126d6364d4a87af18d0be25
Varistus47 <106617396+Varistus47@users.noreply.github.com>
2022-11-28 22:57:29 +0530

    Add files via upload

---
+++ matrix_add.py
@@ -0,0 +1,13 @@
+#pip install numpy
+
+import numpy as gh
+a=gh.array([[1,2,3],[4,5,6]])
+b=gh.array([[3,2,5],[1,0,3]])
+print("array a is:")
+print(a)
+print("array b is:")
+print(b)
+print("matrix addition is:")
+print(a+b) 
+
+
