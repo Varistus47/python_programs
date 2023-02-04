4fd73e1cd042f0f4826146c62713f41ee70c7891
Varistus47 <106617396+Varistus47@users.noreply.github.com>
2023-02-04 21:02:20 +0530

    Add files via upload

---
+++ emo_fun.py
@@ -0,0 +1,13 @@
+import time 
+import os 
+a=['\U0001F600','\U0001F602','\u2764\ufe0f'
+,'\U0001F612','\U0001F910'] 
+print("\n")
+for i in range(5):
+    for i in range(len(a)):
+        print("\n")
+        print(a[i])
+        time.sleep(0.5)
+        os.system("cls")
+
+
